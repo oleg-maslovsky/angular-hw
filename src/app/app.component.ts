408c72bbe131e6b87eeec4a85a60101db39e1581
oleg-tsybulsky <55997596+oleg-tsybulsky@users.noreply.github.com>
2020-12-05 09:30:12 +0200

    0.0.1 beta.1 (#2)
    * update version
    
    * get base version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,18 @@ import { version } from '../../package.json';
 })
 export class AppComponent {
   title = `angular-hw v${version}`;
+
+  constructor() {
+    console.log(version, this.baseVersion(version));
+  }
+
+  private baseVersion(ver: string): string {
+    const convertStringToInt = (item: string): number =>
+      Number.parseInt(item, 10) || 0;
+
+    const arr = version.split('.');
+    return `${convertStringToInt(arr[0])}.${convertStringToInt(
+      arr[1]
+    )}.${convertStringToInt(arr[2])}`;
+  }
 }
